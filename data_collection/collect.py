f390f0d00098c090bda04a37b1b0486ad154aec2
Sumukh Naidu <naidusumukh@gmail.com>
2026-06-09 23:52:20 +0530

    this is my second change

--- data_collection/collect.py
+++ data_collection/collect.py
@@ -9,7 +9,7 @@ mp_draw  = mp.solutions.drawing_utils
 hands    = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7)
 
 # --- SETTINGS ---
-SIGNS        = ["HELLO", "THANK YOU", "WATER", "FOOD", "HELP", "YES", "NO", "PLEASE"]
+SIGNS        = ["HELLO", "THANK YOU", "WATER", "FOOD", "HELP", "YES", "NO", "PLEASE", "GOOD", "MORNING", "HOW", "ARE", "YOU"]
 NUM_SEQUENCES = 80    # recordings per sign
 SEQ_LENGTH    = 30    # frames per recording
 DATASET_PATH  = "data_collection/dataset"
